4bee33a4dbb26edbd948e7ccb10b6f95c7223121
Saulo de Társio <saulodetarsio107@gmail.com>
2019-11-09 14:17:00 -0200

    Adicionando as devidas rotas de websocket para mapa e medidas

--- app/routing.py
+++ app/routing.py
@@ -1,9 +1,9 @@
-# chat/routing.py
 from django.conf.urls import url
 
 from . import consumers
 
 #Necessário para criar as conexões entre Websockets e o core do Django.
 websocket_urlpatterns = [
-    url(r'^ws/app/', consumers.AppConsumer)
+    url(r'^ws/app/medidas', consumers.AppConsumer),
+    url(r'^ws/app/loc', consumers.MapaConsumer),
 ]
