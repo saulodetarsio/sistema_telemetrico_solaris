f970ca52e13b1b80908f8e7d3944f21d9cc47eb9
Saulo de Társio <saulodetarsio107@gmail.com>
2019-11-19 21:24:19 -0300

    Atualizando o código para fazer o arduino receber a mensagem composta com os dados coletados dos sensores de forma correta.

--- source_arduino/teste_mqtt_backup/teste_mqtt_backup.ino
+++ source_arduino/teste_mqtt_backup/teste_mqtt_backup.ino
@@ -2,26 +2,27 @@
 #include<SoftwareSerial.h>
 #include<SD.h>
 #include <Ethernet.h>
-#include <PubSubClient.h>
+//#include <PubSubClient.h>
 #include <SPI.h>
 
 // Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(192,168,0,222); 
-IPAddress server(192,168,0,252);
+//byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+//IPAddress ip(192,168,0,222); 
+//IPAddress server(192,168,0,252);
 
 
 SoftwareSerial ss(8,9);
-EthernetClient ethClient;
-PubSubClient client(ethClient);
+//EthernetClient ethClient;
+//PubSubClient client(ethClient);   
 
-File myFile;
-char mensagem[48];
+//File myFile;
+char mensagem[47];
 byte index = 0;
 
 void setup() {
   Serial.begin(9600);
-  ss.begin(38400);
+  ss.begin(9600);
+  /**
   if (!SD.begin(4)) {
     Serial.println("Falha ao iniciar SD!!!");
     Serial.println("Desligue o arduino e insira um SD!");
@@ -31,13 +32,15 @@ void setup() {
 
    client.setServer(server, 1883);
      Ethernet.begin(mac, ip);
-
+    **/
+    
      delay(2000);
 }
 
 
 void reconnect() {
   // Loop until we're reconnected
+  /**
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
@@ -50,10 +53,10 @@ void reconnect() {
       // Wait 5 seconds before retrying
       delay(5000);
     }
-  }
+  }**/
 }
 
-//[13.5,60.7,82.4,04.7,87.1,-58.156719,-18.896437]
+//[13.5,60.7,82.4,04.7,87.1,-58156719,-18896437]
 void enviar_grandezas_medidas(){
   char medidas[27];
 
@@ -65,26 +68,31 @@ void enviar_grandezas_medidas(){
   medidas[25] =  ']';
   medidas[26] =  '\0';
 
-
-  client.publish("app/medidas", medidas);
+  Serial.print("Grandezas medidas: ");
+  Serial.println(medidas);
+  //client.publish("app/medidas", medidas);
 }
 
 void enviar_localizacao(){
-  char loc[24];
+  char loc[22];
 
   loc[0] = '[';
 
-  for(byte a = 26; a < 48; a++){
+  for(byte a = 26; a < 45; a++){
     loc[a-25] = mensagem[a];
   }
-  loc[22] =  ']';
-  loc[23] =  '\0';
+  loc[20] =  ']';
+  loc[21] =  '\0';
 
- 
-  client.publish("app/loc", loc);
+  
+  Serial.print("Geolocalização medida: ");
+  Serial.println(loc);
+  
+  //client.publish("app/loc", loc);
 }
 
 void salvar_SD() {
+  /**
   //abre o arquivo para edição
   myFile = SD.open("dados.txt", FILE_WRITE);
   
@@ -92,19 +100,20 @@ void salvar_SD() {
   myFile.println(mensagem);
 
   //fecha e salva o arquivo após edição
-  myFile.close();
+  myFile.close(); **/
 }
 
 void loop() {
 
-  if (!client.connected()) {
-    reconnect();
-  }
+  //if (!client.connected()) {
+    //reconnect();
+ // }
   
   while(ss.available()){
     char c = ss.read();
 
-    if(index == 0 && c != '[' && (c)){
+    if((index == 0 && c != '[') || (index == 26 && c == '0')){
+      index = 0;
       continue;
     }
         
@@ -113,17 +122,22 @@ void loop() {
     
     //Fim da leitura da mensagem
     //índice deve ser zerado
-    if(index == 48){
-     
+    if(index == 46){
+
+      mensagem[index] = '\0';
+      
+      Serial.print("Mensagem recebida: ");
+      Serial.println(mensagem);
+      
       index = 0;
-      salvar_SD();
+      //salvar_SD();
       enviar_grandezas_medidas();
       enviar_localizacao();
     
       continue;
     } 
   }
-    client.loop();
+ //   client.loop();
   delay(100);
 
 }
