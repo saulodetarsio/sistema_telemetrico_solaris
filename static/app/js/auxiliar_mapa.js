cc9879209db2f08fe243d42b09128fda55ec5f89
Saulo de Társio <saulodetarsio107@gmail.com>
2019-12-23 00:03:29 -0300

    Adicionando css, js e img da aplicação

---
+++ static/app/js/auxiliar_mapa.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+    var controleMapa = new ControleMapa();
+  
+    /**
+    *   Valores que indicam o local que será renderizado no centro do elemento HTML.        
+    **/
+    var centro_mapa = [-26.254933, -48.707542]
+
+
+    /***
+    *   O valor máximo de zoom do mapa.
+    **/
+    var zoom_maximo = 11;
+    
+
+    /**
+    *   O valor mínimo de zoom do mapa.
+    **/
+    var zoom_minimo =  11;
+
+
+    /** 
+    * O valor inicial de zoom que o mapa será renderizados
+    **/
+    var zoom_inicial = 11;
+
+
+    /**
+    * A forma de exbição de mapa. Satélite, Híbridos, ruas, etc
+    **/
+//var fonte_mapa = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+
+//var fonte_mapa = "../img/mapa/{z}/{x}/{y}.png"
+
+var fonte_mapa = '/static/app/img/mapa/{z}/{x}/{y}.jpg';
+
+controleMapa.criar_mapa("mapa", centro_mapa, zoom_inicial)
+
+var boias = []
+
+$.ajax({
+    method: 'GET',
+    url: 'boias',
+        
+    success:function(responseData){
+        for(var i = 0;  i < responseData.length; i++){
+            var latitude = parseFloat(responseData[i][0])
+            var longitude = parseFloat(responseData[i][1])
+            boias.push([latitude, longitude])
+           controleMapa.renderizar_boia([latitude, longitude]);
+
+        }
+
+        if(boias.length > 0)
+            controleMapa.renderizar_circuito_prova(boias)
+    }
+})
+
+    controleMapa.renderizar_mapa(fonte_mapa, zoom_minimo, zoom_maximo)
+    controleMapa.adicionar_marcador_barco([-22.753036, -41.8922]);
+       
+    var appSocket = new WebSocket("ws://"+ window.location.host +"/ws/app/loc");
+    //Recebendo informaçoes da medidas na página
+    appSocket.onmessage = function(e) {
+      var data = JSON.parse(e.data);
+      var message = data['message'];
+      
+
+      var m = message.substr(3, 19);
+      
+      var t = m.split(",")
+      var lat = t[0].toString()
+      var lng = t[1].toString()   
+
+      var n_la = lat.slice(0, 3)+"."+lat.slice(3, 9)   
+      var n_lo = lng.slice(0, 3)+"."+lng.slice(3, 9)   
+
+
+       controleMapa.atualizar_localizacao_barco([n_la, n_lo])
+    }
+    
+
+})
---
+++ static/app/js/controle_mapa/controle_mapa.js
@@ -0,0 +1,83 @@
+/**
+* Esse objeto contém todas as ações que podem ser feitas em cima do mapa da aplicação
+**/
+function ControleMapa (){	
+
+	this.mapa = L.map('mapa', { zoomControl:false , dragging: false})	
+	this.popup = null
+	this.boias = []
+
+	/**
+	* Cria o objeto mapa para ser manipulado
+	**/
+	this.criar_mapa = function(id, coords,  z){		
+		this.mapa.setView([coords[0], coords[1]], z);		
+		this.mapa.on('click', this.onMapClick);
+	}
+
+	this.onMapClick = function(e){
+		console.log("Lng: "+e.latlng.toString())
+
+	}
+
+	/**
+		Renderiza o mapa com as propriedades passadas por meio dos parâmetros passados para o método.
+	**/
+	this.renderizar_mapa = function(src, zMin,  zMax){
+		L.tileLayer(src, {
+			maxZoom: zMax,
+      		minZoom: zMin,
+      		zoomControl: false
+    	}).addTo(this.mapa);
+	}
+
+	/**
+	* Cria e adiciona no mapa o marcador que representa o barco.
+	**/
+	this.adicionar_marcador_barco = function(coord){
+		var myIcon = L.divIcon({className: 'geolocalizacao'});
+		this.barco = L.marker(coord, {icon: myIcon}).addTo(this.mapa);
+	}
+
+	/**
+	* Adiciona o marcador referêncial para medir a distância de afastamento do barco
+	**/
+	this.adicionar_marcador_referencial = function(coord){
+		var myIcon = L.divIcon({className: 'referencial'});
+		this.barco = L.marker(coord, {icon: myIcon}).addTo(this.mapa);
+	}
+
+	/**
+	* Método que atualiza a coordenada do marcador que representa o barco, dando a ideia de movimento.
+	**/
+	this.atualizar_localizacao_barco = function (coord){
+		this.barco.setLatLng(coord);
+	}
+
+	/**
+	* Método que adiciona as boias que irão delimitar o percurso da prova.
+	**/
+	this.renderizar_boia = function(boia){
+		var myIcon = L.divIcon({className: 'boia'});
+		L.marker(boia, {icon: myIcon}).addTo(this.mapa);
+		this.boias.push(boia)
+	}
+
+
+	this.renderizar_circuito_prova = function(){
+		var coords = []
+		for(var k in this.boias){
+			var latitude = this.boias[k][0]
+			var longitude = this.boias[k][1]
+			coords.push([latitude, longitude])
+		}
+
+		coords.push(this.boias[0])
+		var polygon = L.polygon(coords, {color: 'white', fill: true, 
+			lineJoin: 'round', fillColor: 'none', fillOpacity: 0.5}).addTo(this.mapa);	
+	}
+}
+
+
+
+
